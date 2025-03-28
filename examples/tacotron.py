61637db28f09b100a4247d6f9f5ad1844d6052e4
Ryan Mullins <ryanmullins@google.com>
2025-03-28 15:40:54 +0000

    Final fixes for example verification

--- examples/tacotron.py
+++ examples/tacotron.py
@@ -19,7 +19,6 @@ Tacotron: Towards End-to-End Speech Synthesis
 https://arxiv.org/abs/1703.10135
 """
 
-from sequence_layers import proto as slp
 import sequence_layers as sl
 import tensorflow.compat.v2 as tf
 
@@ -65,60 +64,3 @@ def TacotronDecoder(
       # Predict reduction_factor output frames from the resulting hidden state.
       sl.Dense(target_dimension * reduction_factor),
   ])
-
-
-def TacotronDecoderAsProto(
-    source_name: str, reduction_factor: int = 2, target_dimension: int = 128
-) -> slp.SequenceLayer:
-  """Returns a SequenceLayer protobuf representing a Tacotron decoder."""
-
-  def LSTM(num_units):
-    return slp.RNN(cell=[slp.RNN.Cell(lstm=slp.RNN.LSTMCell(units=num_units))])
-
-  return slp.Serial(
-      layer=[
-          # Squeeze to [b, t // rf, rf, target_dimension].
-          slp.Squeeze(factor=reduction_factor),
-          # Flatten to [b, t // rf, rf * target_dimension]
-          slp.Flatten(),
-          # Pre-net: Apply an information bottleneck to the previous feature
-          # frames.
-          slp.Dense(units=256, activation=slp.Relu()),
-          slp.Dropout(rate=0.5),
-          slp.Dense(units=128, activation=slp.Relu()),
-          slp.Dropout(rate=0.5),
-          # Autoregress on the attention output (cell_output, context_vector).
-          slp.Autoregressive(
-              layer=[
-                  LSTM(256),
-                  # Concatenate cell output with context vector.
-                  slp.Skip(
-                      layer=[
-                          # Attend to the source sequence with
-                          # query = RNN([previous_context_vector,
-                          # previous_output]).
-                          slp.GmmAttention(
-                              source_name=source_name,
-                              num_heads=1,
-                              units_per_head=128,
-                              num_components=5,
-                              monotonic=True,
-                              init_offset_bias=1.0,
-                              init_scale_bias=1.0,
-                          ),
-                          # Flatten the [b, t//2, num_heads, units_per_head] to
-                          # [b, t//2, num_heads * units_per_head].
-                          slp.Flatten(),
-                      ]
-                  ),
-              ]
-          ),
-          # Transform the [attention RNN output, context vector] with 2 residual
-          # RNN layers.
-          slp.Residual(layer=[LSTM(256)]),
-          slp.Residual(layer=[LSTM(256)]),
-          # Predict reduction_factor output frames from the resulting hidden
-          # state.
-          slp.Dense(units=target_dimension * reduction_factor),
-      ]
-  )
--- examples/tacotron_test.py
+++ examples/tacotron_test.py
@@ -13,7 +13,6 @@
 # limitations under the License.
 
 from absl.testing import parameterized
-from sequence_layers import proto
 from sequence_layers import test_util
 import tensorflow.compat.v2 as tf
 
@@ -32,29 +31,6 @@ class TacotronTest(test_util.SequenceLayerTest, parameterized.TestCase):
     l = tacotron.TacotronDecoder(
         source_name, reduction_factor=rf, target_dimension=target_dimension
     )
-
-    # The encoder sequence to attend to.
-    source = self.random_sequence(batch_size, source_length, source_dim)
-    constants = {source_name: source}
-
-    # Only test training=False because the model has dropout.
-    self.verify_contract(l, x, training=False, constants=constants)
-    # Use flex for Einsum.
-    self.verify_tflite_step(
-        l, x, constants=constants, use_flex=True, rtol=5e-6, atol=5e-6
-    )
-
-  def test_tacotron_from_proto(self):
-    batch_size, source_length, source_dim = 2, 3, 5
-    target_length, target_dimension = 7, 11
-    source_name = 'source'
-    # The target sequence to learn.
-    x = self.random_sequence(batch_size, target_length, target_dimension)
-    spec = tacotron.TacotronDecoderAsProto(
-        source_name, reduction_factor=2, target_dimension=target_dimension
-    )
-
-    l = proto.build_sequence_layer(spec)
 
     # The encoder sequence to attend to.
     source = self.random_sequence(batch_size, source_length, source_dim)
